0e09ed2a1d9fed69067486bd7bc73ffd2b506b21
sonkeuu <agatajoannawojcik@gmail.com>
2024-10-15 16:37:33 +0200

    making "Sorting By Tables Machine"

--- other_excercises_and_tests/sorting_by_tables.py
+++ other_excercises_and_tests/sorting_by_tables.py
@@ -1,16 +1,24 @@
+from typing import List, Type
+
+list_of_numbers: list[int] = [102, 504, 315, 3660, 827, 742, 331, 780, 963, 15, 2048]
 
 
-list_of_numbers = list[int]
+def max_number_of_digits(list_of_numbers_to_sort: list[int]):
 
-def item_sorter():
-    ...
+    letter = str(max(list_of_numbers_to_sort))
+    #   print(letter)
+    counted = len(letter)
+    return counted
+
+    #   for number in list_of_numbers:
+    #   list_of_numbers.index(number)
+
+
+def digits_number_equalize():
+    digits_searched_number = max_number_of_digits(list_of_numbers)
+
+
 
 if __name__ == '__main__':
-
-
-
-
-
-
-
+    print(max_number_of_digits(list_of_numbers))
 
