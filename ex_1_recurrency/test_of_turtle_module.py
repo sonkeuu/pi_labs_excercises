8473f605071a4110190959d6128cfb3456410a1a
sonkeuu <agatajoannawojcik@gmail.com>
2024-10-16 09:44:34 +0200

    Turtle Module -> Task_A10 & Task_A1 in progress

--- ex_1_recurrency/test_of_turtle_module.py
+++ ex_1_recurrency/test_of_turtle_module.py
@@ -53,18 +53,19 @@ class Task_A1(Shape):
     def shape_inside(self):
 
         starting_position_square(self._distance)
-
-        square(self._distance)
+        #square(self._distance)
         distance_2 = self._distance
-        distance_2 = distance_2 / 2
 
-        for i in range(4):
-            turtle.penup()
-            turtle.goto(0, 0)
-            turtle.forward(distance_2)
-            turtle.right(90)
-            turtle.pendown()
-            square(distance_2)
+        for j in range(4):
+            distance_2 = distance_2 / 2
+
+            for i in range(4):
+                turtle.penup()
+                turtle.goto(0, 0)
+                turtle.forward(distance_2)
+                turtle.right(90)
+                turtle.pendown()
+                square(distance_2)
 
     def draw_the_shape(self):
 
@@ -79,21 +80,37 @@ class Task_A10(Shape):
     def __init__(self, set_the_distance_10):
         self._distance_10 = set_the_distance_10
 
-    def shape_inside(self):
+    def shape_inside(self, distance_spiral_gap: int):
 
         starting_position_triangle(self._distance_10)
-        self.triangle_spiral()
+        self._distance_spiral_gap = distance_spiral_gap
+
+        for i in range(10):
+            turtle.left(120)
+            turtle.forward(self._distance_10 - self._distance_spiral_gap)
+
+
+
+
+
 
     def triangle_spiral(self):
 
         starting_position_triangle(self._distance_10)
-        distance_spiral_diff = self._distance_10 / 40
-        h_value = int(self._distance_10 / 13)
 
-        for i in range(h_value):
+        distance_spiral_gap = 10
+
+        for i in range(30):
             turtle.left(120)
-            distance_spiral_diff = distance_spiral_diff + (self._distance_10 / 40)
-            turtle.forward(self._distance_10 - distance_spiral_diff)
+            distance_spiral_gap = distance_spiral_gap + 10
+            turtle.forward(self._distance_10 - distance_spiral_gap)
+
+
+
+class Task_A2(Shape):
+
+    def __init__(self):
+        ...
 
 
 
@@ -101,8 +118,9 @@ class Task_A10(Shape):
 
 screen = turtle.Screen()
 
-#triangle(400)
-#square(400)
+if __name__ == '__main__':
+    #triangle(400)
+    #square(400)
 
-Task_A10(500).triangle_spiral()
+    Task_A1(300).shape_inside()
 
