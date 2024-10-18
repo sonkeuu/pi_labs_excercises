bb92d8ec3ccc4c78d59d55c157037538b90628c9
sonkeuu <agatajoannawojcik@gmail.com>
2024-10-18 22:23:34 +0200

    Turtle Module ---> task 2 still undone

--- ex_1_recurrency/task_2_lab_1.py
+++ ex_1_recurrency/task_2_lab_1.py
@@ -1,22 +1,39 @@
 import turtle
 from turtle import *
 
-turtle.speed(15)
+turtle.speed(2)
 
 def task_2_lab_1(x, minimum_x):
     if x < minimum_x:
         return
     for i in range(4):
-        forward(x)
-        left(90)
-        forward(x)
-        left(180)
-        task_2_lab_1(x/1.5, minimum_x)
+        for j in range(4):
+            forward(x)
+            left(90)
+        forward(x/4)
+        right(90)
+        task_2_lab_1(x / 2, minimum_x)
+
+
+
+def test(x, minx):
+    if x< minx:
+        return
+    for i in range(4):
+        for j in range(6):
+            forward(x)
+            left(90)
+        forward(x/4)
+        right(90)
+        test(x/2, minx)
+    test(x, minx)
+
 
 
 
 if __name__ == '__main__':
 
     #   minimum_x   --->  x / 16
-    task_2_lab_1(256, 128)
+    test(100, 10)
+    #task_2_lab_1(200, 25)
     turtle.done()
--- ex_1_recurrency/task_3_lab_1.py
+++ ex_1_recurrency/task_3_lab_1.py
@@ -3,6 +3,7 @@ from turtle import *
 
 turtle.speed(50)
 turtle.left(90)
+turtle.backward(200)
 
 def task_3_lab_1(x, minimum_x):
     if x < minimum_x:
